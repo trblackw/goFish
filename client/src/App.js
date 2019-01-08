9ecff59b49e8737e3bf1d22c330d10a84d60e0bd
Tucker <trblackw@ncsu.edu>
2019-01-08 11:52:10 -0500

    set up context and passed relevant data to app via provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import "./App.css";
 import { Router } from "@reach/router";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import UserContext from "./state/context";
+import UserReducer from "./state/reducer";
 
-const App = () => (
-  <>
-    <Nav />
-    <Router>
-      <Landing path="/" />
-    </Router>
-  </>
-);
+const App = () => {
+  const initialState = useContext(UserContext);
+  const [state, dispatch] = useReducer(UserReducer, initialState);
+  return (
+    <>
+      <UserContext.Provider value={{ state, dispatch }}>
+        <Nav />
+        <Router>
+          <Landing path="/" />
+          <Login path="login" />
+          <Register path="register" />
+        </Router>
+      </UserContext.Provider>
+    </>
+  );
+};
 
 export default App;
