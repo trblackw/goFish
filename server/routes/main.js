43a7811133a02c6dcaa4a466d1c8773c6ec5362f
Tucker <trblackw@ncsu.edu>
2019-01-09 15:19:07 -0500

    temp modification to main.js route for demo searching

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const waApi = require("../wolfram_api/index");
+const fetch = require("node-fetch");
 
 router.get("/fishing", (req, res) => {
   const { search } = req.query;
-  waApi
-    .getFull(search)
-    .then(data => res.send(JSON.stringify(data)))
-    .catch(err => console.error(err));
+  const searchLength = search.split(" ").length;
+  if (searchLength <= 6) {
+    waApi
+      .getShort({ input: search, output: "json", includepodid: "Result" })
+      .then(data => res.send(JSON.stringify(data)))
+      .catch(err => res.send(err));
+  } else {
+    waApi
+      .getFull(search)
+      .then(data => res.send(JSON.stringify(data)))
+      .catch(err => res.send(err));
+  }
 });
 
 module.exports = router;
