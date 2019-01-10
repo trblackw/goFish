97c2ea651824d649766510a98d09f7f148bbd2c2
Tucker <trblackw@ncsu.edu>
2019-01-10 15:15:41 -0500

    extracted out passport logic

---
+++ server/config/passport.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("../models/user");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "user[username]",
+      passwordField: "user[password]"
+    },
+    (username, password, done) => {
+      User.findOne({ username })
+        .then(user => {
+          if (!user || !user.validatePassword(password)) {
+            return done(null, false, {
+              errors: { "username or password": "is invalid" }
+            });
+          }
+          return done(null, user);
+        })
+        .catch(done);
+    }
+  )
+);
